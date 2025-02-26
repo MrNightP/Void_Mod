2ea73cd29ad47b4ec8ad8c1115d1ee2811089716
MrNightP <mrdaniilplayer@gmail.com>
2025-02-26 21:01:00 +0200

    uPd

---
+++ src/main/java/net/night/voidmod/VoidMod.java
@@ -0,0 +1,65 @@
+package net.night.voidmod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.night.voidmod.block.ModBlocks;
+import net.night.voidmod.item.CreativeModTabs;
+import net.night.voidmod.item.ModItems;
+import net.night.voidmod.loot.ModLootModifiers;
+import net.night.voidmod.network.DashPacket;
+import net.night.voidmod.network.EffectPacket;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(VoidMod.MOD_ID)
+public class VoidMod {
+    public static final String MOD_ID = "voidmod";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public VoidMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        CreativeModTabs.register(modEventBus);
+
+        modEventBus.addListener(this::commonSetup);
+
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
+        EffectPacket.register();
+        DashPacket.register();
+
+        ModLootModifiers.register(modEventBus);
+
+        MinecraftForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            // Настройка клиентской части
+        }
+    }
+}
